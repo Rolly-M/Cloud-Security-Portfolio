59440d2fbdf59e620768ef01bd03f3774b137724
MOUGOUE KAKANOU ROLLY DAVANY <99815109+Rolly-M@users.noreply.github.com>
2026-03-24 19:46:38 -0400

    Deleted duplicates in variables file

--- 1 - AWS-Secure-VPC/TF scripts/variables.tf
+++ 1 - AWS-Secure-VPC/TF scripts/variables.tf
@@ -54,66 +54,16 @@ variable "key_name" {
   default     = "my-key-pair"
 }
 
-# variables.tf (ADD THESE TO EXISTING FILE)
-
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
   default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-  default     = "production"
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
-
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "bastion_instance_type" {
-  description = "EC2 instance type for bastion host"
-  type        = string
-  default     = "t3.micro"
-}
-
 variable "private_instance_type" {
   description = "EC2 instance type for private instances"
   type        = string
   default     = "t3.micro"
-}
-
-variable "allowed_ssh_cidr" {
-  description = "CIDR block allowed to SSH to bastion (your IP)"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "key_name" {
-  description = "SSH key pair name"
-  type        = string
-  default     = "my-key-pair"
 }
 
 # NEW VARIABLES
